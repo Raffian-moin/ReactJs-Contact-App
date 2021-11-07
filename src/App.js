78a536687c987ec34aa0b7084e91ad27f772dc9d
moin <moin.uddin@technovista.com.bd>
2021-11-07 17:35:27 +0600

    added dynamic list and working with localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddContact from "./Components/AddContact";
 import ContactList from "./Components/ContactList";
 import Header from "./Components/Header";
@@ -6,10 +6,22 @@ import Header from "./Components/Header";
 import "./App.css";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
     console.log(contact);
   }
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) {
+      setContacts(retrieveContacts);
+    }
+  }, []);
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
   return (
     <div className="ui container">
       <Header />
